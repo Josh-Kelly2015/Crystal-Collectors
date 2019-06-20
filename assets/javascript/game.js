9bda3d953cac15f8d89787b9e73053d534aab19b
Joshua Kelly <jdkelly2015@gmail.com>
2019-06-20 09:50:56 -0400

    wrote actual instructions, pseudo coded JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+//4 images of crystals that are buttons with IDs and a value
+//player is displayed a random number(19-120) in a specificed div 
+//these buttons have hidden values(1-12) so that when theyre clicked they display that value
+//player must add values together to equal the random number displayed by clicking images
+//these values are only discovered by the user after clicking the crystal
+//every clicked crystal updates the div this number is displayed in with the firstvalue + secondvalue
+//player wins++ if totalScore===goal
+//players loses++ if totalScore < goal 
+//game restarts after every win/lose
+//new game means new random number, new crystal values, score/score_counter reset 0
+//const wins/losses even after new games. 
+
