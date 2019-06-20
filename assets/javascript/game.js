d39e45bba7966b037f5c4460eb7aed17ad059fb3
Joshua Kelly <jdkelly2015@gmail.com>
2019-06-20 18:52:07 -0400

    css is kinda fucked but oh well. my buttons work and i have a good function to apply several places

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,25 @@
 //4 images of crystals that are buttons with IDs and a value
+
 //player is displayed a random number(19-120) in a specificed div 
-//these buttons have hidden values(1-12) so that when theyre clicked they display that value
+
+
+
+
+    $("button").on("click", function (min, max) {
+        min = Math.ceil(19);
+        max = Math.floor(120);
+        randomize = document.getElementById("goal").innerHTML = Math.floor(Math.random() * (max - min + 1)) + min;
+        return randomize
+    });
+
+
+
+
+
+
+//these buttons have hidden values (1-12) so that when theyre clicked they display that value
+
+
 //player must add values together to equal the random number displayed by clicking images
 //these values are only discovered by the user after clicking the crystal
 //every clicked crystal updates the div this number is displayed in with the firstvalue + secondvalue
