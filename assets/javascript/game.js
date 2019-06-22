7029724ba85cf561f55a8dc0121445c5fafaaed3
Joshua Kelly <jdkelly2015@gmail.com>
2019-06-22 02:11:09 -0400

    i was able to add the proper functions to my img tags. i am giving each image a value then displaying it in the div ID score. i also have a randomized goal set each time the page loads. tomorrow i will add a function that adds my values. i will also need to add a match function to match my goal with my score. i need to reset and rename all numbers upon loss/win. increment wins/losses.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,60 @@
 //4 images of crystals that are buttons with IDs and a value
 
 //player is displayed a random number(19-120) in a specificed div 
+//variables i made global for some reason because for some reason it reads as undefined 
+var min = Math.ceil(19);
+var max = Math.floor(120);
+
+//my function to randomize numbers between a certain range
+function goalNumber(min, max) {
+    var max = Math.floor(120);
+    var result = Math.floor(Math.random() * (max - min + 1)) + min;
+    return result
+};
 
 
+//displays a randomized number to my goal div
+document.getElementById("goal").innerHTML = goalNumber(min, max);
+
+//another function to generate random numbers 1-12 for my score div
+function score(min, max) {
+    var min = Math.ceil(1);
+    var max = Math.floor(12);
+    var result = Math.floor(Math.random() * (max - min + 1)) + min;
+    return result
+};
+
+//sets value to my blueCrystal img
+$("#blueCrystal").attr("value", score(min, max));
+blueCrystalValue = $("#blueCrystal").attr("value");
+
+console.log(blueCrystalValue)
+
+//sets value for greenCrystal
+$("#greenCrystal").attr("value", score(min, max));
+greenCrystalValue = $("#greenCrystal").attr("value");
 
 
-    $("button").on("click", function (min, max) {
-        min = Math.ceil(19);
-        max = Math.floor(120);
-        randomize = document.getElementById("goal").innerHTML = Math.floor(Math.random() * (max - min + 1)) + min;
-        return randomize
-    });
+console.log(greenCrystalValue);
 
+//sets value for purpleCrystal
+$("#purpleCrystal").attr("value", score(min, max));
+purpleCrystalValue = $("#purpleCrystal").attr("value");
 
+console.log(purpleCrystalValue);
 
+//sets value for rainbowCrystal
+$("#rainbowCrystal").attr("value", score(min, max));
+var rainbowCrystalValue = $("#rainbowCrystal").attr("value");
 
+console.log(rainbowCrystalValue);
 
+//this function dispalys the random number chosen for the img values. i have to add these together by keeping the previous value always displayed
+$("img[value]").on("click", function () {
+    var totalScore = $(this).attr("value");
 
-//these buttons have hidden values (1-12) so that when theyre clicked they display that value
+    return document.getElementById("score").innerHTML = totalScore
+})
 
 
 //player must add values together to equal the random number displayed by clicking images
